e577109e10426a42fd7394dd393cfd8c626cf283
xurui203 <xurui203@gmail.com>
2014-07-06 16:35:36 -0700

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Collegepilot::Application.config.secret_key_base = '69c35eca8d2e083984b60845bbd1a89ddcd66b70b373d7c3fc67f10af103f39b4d4f1addce32604f55416e6f91fd266c83d1488d66df1b020c230c9058b582bd'
