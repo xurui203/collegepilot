cc4336a48daafbe17a7f48c12dceb2f5539d6cda
xurui203 <xurui203@gmail.com>
2014-07-14 22:20:18 -0700

    static pages done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Collegepilot::Application.routes.draw do
   get "student/new"
-  get "static_pages/home"
-  get "static_pages/about"
-  get "static_pages/contact"
+
+  root 'static_pages#home'
+  match'/signup', to: 'student#new', via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,4 +64,4 @@ Collegepilot::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
